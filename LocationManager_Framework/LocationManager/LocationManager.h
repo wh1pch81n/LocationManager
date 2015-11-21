ac61810ffe92e063531e8abef5df891f4aa3d8b9
Derrick Ho <derrickho328@gmail.com>
2015-11-21 00:18:29 -0500

    refactor

---
+++ LocationManager_Framework/LocationManager/LocationManager.h
@@ -0,0 +1,19 @@
+//
+//  LocationManager.h
+//  LocationManager
+//
+//  Created by Derrick  Ho on 11/20/15.
+//  Copyright © 2015 Derrick  Ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocationManager.
+FOUNDATION_EXPORT double LocationManagerVersionNumber;
+
+//! Project version string for LocationManager.
+FOUNDATION_EXPORT const unsigned char LocationManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocationManager/PublicHeader.h>
+
+//#import <LocationManager/LocationManager-swift.h>
